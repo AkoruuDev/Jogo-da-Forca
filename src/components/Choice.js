9ec64fc1818db1795b6991f319ce10a921130128
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-22 19:22:30 -0300

    FEAT [Choice Word]: Get word choiced and turn off ContainerWords display

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -1,17 +1,27 @@
 import words from "../services/words";
 import styled from 'styled-components';
+import { useState } from "react";
 
-function Word({word}) {
+function start(word, setShow) {
+    console.log(word);
+    setShow(false);
+}
+
+function Word({word, setShow}) {
     return(
-        <WordBox>{word}</WordBox>
+        <WordBox onClick={() => start(word, setShow)}>{word}</WordBox>
     )
 }
 
 export default function Choice() {
+    const [show, setShow] = useState(false)
     return (
-        <WordContainer>
-            {words.map((word, i) => <Word key={i} word={word} />)}
-        </WordContainer>
+        <>
+            <Button onClick={() => setShow(!show)}>Escolher palavra</Button>
+            <WordContainer show={show}>
+                {words.map((word, i) => <Word key={i} word={word} setShow={setShow}/>)}
+            </WordContainer>
+        </>
     )
 }
 
@@ -27,7 +37,7 @@ const WordContainer = styled.div`
     top: calc(50vh - 45vh);
     left: calc(50vh - 45vh);
 
-    display: flex;
+    display: ${props => props.show ? 'flex' : 'none'};
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: space-evenly;
@@ -48,5 +58,29 @@ const WordBox = styled.span`
     &:hover {
         cursor: pointer;
         background-color: #F5F5F5;
+        border-color: #E6E6FA;
+        box-shadow: 0 4px 10px 4px gray;
+    }
+`
+
+const Button = styled.div`
+    height: 50px;
+    width: 170px;
+    background-color: green;
+    border-radius: 30px;
+    color: #f6f6fa;
+    font-weight: 700;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    position: fixed;
+    top: 3vh;
+    right: 5vw;
+
+    &:hover {
+        cursor: pointer;
+        box-shadow: 0 4px 10px 4px gray;
     }
 `
