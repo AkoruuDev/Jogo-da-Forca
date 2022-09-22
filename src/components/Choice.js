2692e04073fcf80a97e5196c53c0597015afe345
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-21 23:44:36 -0300

    STYLE [Words]: Styled words choice on App

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -1,15 +1,45 @@
 import words from "../services/words";
+import styled from 'styled-components';
 
 function Word({word}) {
     return(
-        <>{word}</>
+        <WordBox>{word}</WordBox>
     )
 }
 
 export default function Choice() {
     return (
-        <div>
+        <WordContainer>
             {words.map((word, i) => <Word key={i} word={word} />)}
-        </div>
+        </WordContainer>
     )
 }
+
+const WordContainer = styled.div`
+    width: 40vw;
+    height: 98vh;
+    padding: 20px 20px 40px;
+    background-color: aqua;
+
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    overflow: auto;
+    &::-webkit-scrollbar{
+        opacity: 1;
+    }
+
+`
+
+const WordBox = styled.span`
+    padding: 8px;
+    margin: 10px;
+    border-radius: 10px;
+    background-color: #E6E6FA;
+
+    &:hover {
+        cursor: pointer;
+        background-color: #F5F5F5;
+    }
+`
