8a34a9d7a60397a9b1a03294517a333a763ca8c9
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-22 00:06:07 -0300

    STYLE [Global]: Creat a globalStyle.js file; update boxcontainer style on App

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import GlobalStyle from "../style/globalStyles";
 import Choice from "./Choice";
 import Gallow from "./Gallow";
 import Keyboard from "./Keyboard";
@@ -6,6 +7,7 @@ import Kick from "./Kick";
 export default function App() {
     return (
         <>
+            <GlobalStyle />
             <Choice />
             <Gallow />
             <Keyboard />
--- src/components/Choice.js
+++ src/components/Choice.js
@@ -16,10 +16,16 @@ export default function Choice() {
 }
 
 const WordContainer = styled.div`
-    width: 40vw;
-    height: 98vh;
-    padding: 20px 20px 40px;
-    background-color: aqua;
+    width: 90vw;
+    height: 90vh;
+    padding: 20px;
+    border-radius: 30px;
+    background-color: #FFFAFA;
+    box-shadow: 0 4px 10px 4px gray;
+
+    position: fixed;
+    top: calc(50vh - 45vh);
+    left: calc(50vh - 45vh);
 
     display: flex;
     flex-direction: row;
@@ -37,6 +43,7 @@ const WordBox = styled.span`
     margin: 10px;
     border-radius: 10px;
     background-color: #E6E6FA;
+    font-family: 'Josefin Sans', sans-serif;
 
     &:hover {
         cursor: pointer;
