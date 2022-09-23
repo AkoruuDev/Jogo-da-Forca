bf92e825730961ec5bc66e18eb74079432dc658d
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-22 21:09:20 -0300

    STYLE [Keyboard]: Create keyboard and show on window with style

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,5 +1,56 @@
-export default function Keyboard() {
+import styled from "styled-components"
+
+import alphabet from "../services/alphabet.js"
+
+function Alphabet({letter}) {
     return (
-        <>This is the Keyboard part page</>
+        <Letter>{letter}</Letter>
     )
 }
+
+export default function Keyboard() {
+    return (
+        <AlphabetConteiner>
+            {alphabet.map((letter, i) => <Alphabet key={i} letter={letter} />)}            
+        </AlphabetConteiner>
+    )
+}
+
+const AlphabetConteiner = styled.div`
+    width: 80vw;
+    height: 30vh;
+    // background-color: red; // ---------------------------
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+
+    position: fixed;
+    bottom: 2vh;
+    right: 10vw;
+
+    overflow: auto;
+    &::-webkit-scrollbar {
+        display: none;
+    }
+`
+
+const Letter = styled.div`
+    width: 30px;
+    height: 30px;
+    margin: 20px;
+    background-color: #C0C0C0;
+    border-radius: 8px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 700;
+
+    &:hover {
+        cursor: pointer;
+        background-color: #DCDCDC;
+        box-shadow: 0 4px 10px 2px gray;
+    }
+`
