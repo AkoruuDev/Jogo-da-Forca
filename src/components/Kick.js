af944058219fee03d8f4e2a62a04977572b0baf2
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-25 23:44:18 -0300

    REFACTOR [Console.log]: Remove console.log from Game

--- src/components/Kick.js
+++ src/components/Kick.js
@@ -20,31 +20,17 @@ function KickWord({ letter, theseLetter, setTheseLetter }) {
 }
 
 export default function Kick({ kick, theseLetter, setTheseLetter, gallow, setGallow }) {
-    const [haveTheLetter, setHaveTheLetter] = useState(false);
-    let count = 0;
     useEffect(() => {
-        console.log('kick[i]');
+        let count = 0;
         for (let i = 0; i <= kick.length; i ++) {
-            console.log(kick[i]);
-            console.log(theseLetter);
-            console.log(kick[i] === theseLetter);
             if(kick[i] !== theseLetter) {
                 count = count+1
             }
         }
         if (count !== kick.length) {
-            console.log(gallow)
             setGallow(gallow + 1)
-            console.log('não tem')
-            console.log(gallow)
         }
-    }, [theseLetter])
-    
-    /*useEffect(() => {
-        
-    }, [theseLetter && haveTheLetter])*/
-    
-    
+    }, [theseLetter])    
 
     console.log(kick)
     return (
